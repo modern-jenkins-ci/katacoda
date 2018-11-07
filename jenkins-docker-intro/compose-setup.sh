3a76f0269644a9b687bdc93cf8858e3a5960dc95
Ernesto Ojeda <ernesto.ojeda@intel.com>
2018-11-07 11:08:05 -0700

    update for dynamic jenkins master

--- jenkins-docker-intro/compose-setup.sh
+++ jenkins-docker-intro/compose-setup.sh
@@ -8,6 +8,8 @@ if [ "$ENV" == "HOST1" ]; then
 fi
 
 if [ "$ENV" == "HOST2" ]; then
+  export JENKINS_MASTER=https://[[HOST_SUBDOMAIN]]-8080-[[KATACODA_HOST]].environments.katacoda.com:8080
+  
   ssh root@host01 "cat /root/docker-compose-agent.yml" > /tmp/jenkins/docker-compose.yml
   cd /tmp/jenkins
   clear
