b59c635fa89a5e685f8b57c432ada13ee3f5938a
Ernesto Ojeda <ernesto.ojeda@intel.com>
2018-11-07 13:30:51 -0700

    remove port 8080

--- jenkins-docker-intro/compose-setup.sh
+++ jenkins-docker-intro/compose-setup.sh
@@ -1,5 +1,5 @@
 mkdir -p /tmp/jenkins/data
-export JENKINS_MASTER=https://[[HOST_SUBDOMAIN]]-8080-[[KATACODA_HOST]].environments.katacoda.com:8080
+export JENKINS_MASTER=https://[[HOST_SUBDOMAIN]]-8080-[[KATACODA_HOST]].environments.katacoda.com
 
 if [ "$ENV" == "HOST1" ]; then
   ssh root@host01 "cat /root/docker-compose-master.yml" > /tmp/jenkins/docker-compose.yml
