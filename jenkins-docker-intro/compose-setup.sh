f42371d185c9d5ad371cb14391c8818b5740ffd2
Ernesto Ojeda <ernesto.ojeda@intel.com>
2018-11-07 10:55:31 -0700

    update script

--- jenkins-docker-intro/compose-setup.sh
+++ jenkins-docker-intro/compose-setup.sh
@@ -2,11 +2,10 @@ mkdir -p /tmp/jenkins/data
 
 if [ "$ENV" == "HOST1" ]; then
   ssh root@host01 "cat /root/docker-compose-master.yml" > /tmp/jenkins/docker-compose.yml
-fi
-
-if [ "$ENV" == "HOST2" ]; then
+else
   ssh root@host02 "cat /root/docker-compose-agent.yml" > /tmp/jenkins/docker-compose.yml
 fi
+
 cd /tmp/jenkins
 clear
 pwd
