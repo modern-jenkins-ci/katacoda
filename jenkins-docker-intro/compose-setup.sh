afdf35e7dfd4b1537a8a50807d85b6bc8126fa13
Ernesto Ojeda <ernesto.ojeda@intel.com>
2018-11-07 10:56:28 -0700

    try this

--- jenkins-docker-intro/compose-setup.sh
+++ jenkins-docker-intro/compose-setup.sh
@@ -2,10 +2,14 @@ mkdir -p /tmp/jenkins/data
 
 if [ "$ENV" == "HOST1" ]; then
   ssh root@host01 "cat /root/docker-compose-master.yml" > /tmp/jenkins/docker-compose.yml
-else
-  ssh root@host02 "cat /root/docker-compose-agent.yml" > /tmp/jenkins/docker-compose.yml
+  cd /tmp/jenkins
+  clear
+  pwd
 fi
 
-cd /tmp/jenkins
-clear
-pwd
+if [ "$ENV" == "HOST2" ]; then
+  ssh root@host02 "cat /root/docker-compose-agent.yml" > /tmp/jenkins/docker-compose.yml
+  cd /tmp/jenkins
+  clear
+  pwd
+fi
