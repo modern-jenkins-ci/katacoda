dfd7dc9ec1f30eda74ed87d1a0eea4c7e878b7f0
Ernesto Ojeda <ernesto.ojeda@intel.com>
2018-11-07 13:27:25 -0700

    setup jenkins master from host1

--- jenkins-docker-intro/compose-setup.sh
+++ jenkins-docker-intro/compose-setup.sh
@@ -1,4 +1,5 @@
 mkdir -p /tmp/jenkins/data
+export JENKINS_MASTER=https://[[HOST_SUBDOMAIN]]-8080-[[KATACODA_HOST]].environments.katacoda.com:8080
 
 if [ "$ENV" == "HOST1" ]; then
   ssh root@host01 "cat /root/docker-compose-master.yml" > /tmp/jenkins/docker-compose.yml
@@ -8,8 +9,6 @@ if [ "$ENV" == "HOST1" ]; then
 fi
 
 if [ "$ENV" == "HOST2" ]; then
-  export JENKINS_MASTER=https://[[HOST_SUBDOMAIN]]-8080-[[KATACODA_HOST]].environments.katacoda.com:8080
-  
   ssh root@host01 "cat /root/docker-compose-agent.yml" > /tmp/jenkins/docker-compose.yml
   cd /tmp/jenkins
   clear
