e5d0404d6f6f20890727cf31af3dd9e1991a44f5
Ernesto Ojeda <ernesto.ojeda@intel.com>
2018-11-07 21:34:35 -0700

    update master URL

--- jenkins-docker-intro/compose-setup.sh
+++ jenkins-docker-intro/compose-setup.sh
@@ -1,5 +1,5 @@
 mkdir -p /tmp/jenkins/data
-export JENKINS_MASTER=http://172.17.0.42:8080
+export JENKINS_MASTER=http://[[HOST1_IP]]:8080
 
 if [ "$ENV" == "HOST1" ]; then
   ssh root@host01 "cat /root/docker-compose-master.yml" > /tmp/jenkins/docker-compose.yml
