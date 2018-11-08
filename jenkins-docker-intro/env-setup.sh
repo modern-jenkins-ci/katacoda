7244bfae9f799088caef738ccd9cf13a6675519c
Ernesto Ojeda <ernesto.ojeda@intel.com>
2018-11-07 21:50:26 -0700

    use ssh

---
+++ jenkins-docker-intro/env-setup.sh
@@ -0,0 +1,20 @@
+if [ "$ENV" == "HOST1" ]; then
+host_commands=(
+"mkdir -p /tmp/jenkins/data"
+"cp /root/docker-compose-master.yml /tmp/jenkins/docker-compose.yml"
+)
+  all_commands=$(awk -v sep=' && ' 'BEGIN{ORS=OFS="";for(i=1;i<ARGC;i++){print ARGV[i],ARGC-i-1?sep:""}}' "${host_commands[@]}")
+
+  ssh root@host01 "$all_commands"
+  clear
+  ssh root@host01
+fi
+
+if [ "$ENV" == "HOST2" ]; then
+  export JENKINS_MASTER=http://[[HOST1_IP]]:8080
+  mkdir -p /tmp/jenkins/data
+  ssh root@host01 "cat /root/docker-compose-agent.yml" > /tmp/jenkins/docker-compose.yml
+  cd /tmp/jenkins
+  clear
+  pwd
+fi
