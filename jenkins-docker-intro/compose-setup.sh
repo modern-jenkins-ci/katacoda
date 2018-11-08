d0d619178fbceceff960513218c959115db6d3f3
Ernesto Ojeda <ernesto.ojeda@intel.com>
2018-11-07 20:06:37 -0700

    update to use 172.17.0.42 IP

--- jenkins-docker-intro/compose-setup.sh
+++ jenkins-docker-intro/compose-setup.sh
@@ -1,5 +1,5 @@
 mkdir -p /tmp/jenkins/data
-export JENKINS_MASTER=https://[[HOST_SUBDOMAIN]]-8080-[[KATACODA_HOST]].environments.katacoda.com
+export JENKINS_MASTER=http://172.17.0.42:8080
 
 if [ "$ENV" == "HOST1" ]; then
   ssh root@host01 "cat /root/docker-compose-master.yml" > /tmp/jenkins/docker-compose.yml
